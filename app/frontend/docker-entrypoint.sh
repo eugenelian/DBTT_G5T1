75a3db015d6ec00bd2699ca8a7306257d07a5988
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2026-03-31 11:37:05 +0800

    fix: fixed default path value

--- app/frontend/docker-entrypoint.sh
+++ app/frontend/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Default value if not provided
-: "${VITE_API_URL:=http://dbttg5t1backend-production:8000}"
+: "${VITE_API_URL:=https://dbttg5t1backend-production:8000}"
 
 # Export so envsubst can access it as a child process
 export VITE_API_URL
